5899632ed8dcdfa840999df4de1765ce529592c4
narakuhub <narakuhub@gmail.com>
2026-09-02 03:42:08 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,8 @@
 pcall(function()
+    local infoSrc = game:HttpGet("https://raw.githubusercontent.com/narakuhub/plugin-tools/refs/heads/main/information.lua")
+    local infoFn = loadstring(infoSrc)
+    if infoFn then infoFn() end
+
     local gizmoSrc = game:HttpGet("https://raw.githubusercontent.com/narakuhub/plugin-tools/refs/heads/main/tools/ExtendedGizmo.lua")
     local gizmoFn = loadstring(gizmoSrc)
     if gizmoFn then gizmoFn() end
