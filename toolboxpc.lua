b4db49e63f9564d068232c4bd069f833f424c323
narakuhub <narakuhub@gmail.com>
2026-09-18 07:41:13 +0700

    Update toolboxpc.lua

--- toolboxpc.lua
+++ toolboxpc.lua
@@ -2714,6 +2714,11 @@ local MenuFeatureList = {
         RawLink = "https://raw.githubusercontent.com/narakuhub/plugin-tools/refs/heads/main/tools/terrain.lua"
     },
 	{
+        Name = "TERRAIN V0.02",
+        Icon = "rbxassetid://129361331543944",
+        RawLink = "https://raw.githubusercontent.com/narakuhub/plugin-tools/refs/heads/main/tools/terrain2.lua"
+    },
+	{
         Name = "EDITOR CODE",
         Icon = "rbxassetid://130150477351734",
         RawLink = "https://raw.githubusercontent.com/narakuhub/narsenvr-editorcode/refs/heads/main/load.lua"
